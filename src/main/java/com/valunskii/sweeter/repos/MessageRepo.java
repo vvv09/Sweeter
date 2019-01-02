d3d85f56acf0b3813d42882b648d3b04d8fd4c9e
Vadim Valunskii <dvvv09@gmail.com>
2019-01-02 13:03:28 +0300

    Add DataBase

---
+++ src/main/java/com/valunskii/sweeter/repos/MessageRepo.java
@@ -0,0 +1,10 @@
+package com.valunskii.sweeter.repos;
+
+import com.valunskii.sweeter.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Integer> {
+    List<Message> findByTag(String tag);
+}
