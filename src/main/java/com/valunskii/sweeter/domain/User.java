50749b2bc0a1a88c05e3127fe45964e24263c2c9
Vadim Valunskii <dvvv09@gmail.com>
2019-01-04 15:30:45 +0300

    Fix email validation

--- src/main/java/com/valunskii/sweeter/domain/User.java
+++ src/main/java/com/valunskii/sweeter/domain/User.java
@@ -19,9 +19,7 @@ public class User implements UserDetails {
     private String username;
     @NotBlank(message = "Password cannot be empty")
     private String password;
-    @Transient
-    @NotBlank(message = "Password confirmation cannot be empty")
-    private String password2;
+
     private boolean active;
 
     @Email(message = "Email is not correct")
@@ -36,14 +34,6 @@ public class User implements UserDetails {
 
     public boolean isAdmin() {
         return roles.contains(Role.ADMIN);
-    }
-
-    public String getPassword2() {
-        return password2;
-    }
-
-    public void setPassword2(String password2) {
-        this.password2 = password2;
     }
 
     public Long getId() {
